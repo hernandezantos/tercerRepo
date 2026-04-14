e60a75e92cb9a3cde36f4ed98a346e0fb1f3d8dc
xd1 <hernandezantos@gmail.com>
2026-04-14 00:59:35 -0600

    primer lanzamiento para este nuevo paquete

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Fernando Dev",                         # Tu nombre
+    author_email="correo@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/hernandezantos/tercerRepo",     # URL del proyecto
+)
+
